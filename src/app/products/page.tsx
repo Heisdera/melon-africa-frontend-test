2c3cde9e4aa0d5daca039a91a472fb3dcdf404b1
Raphael Wisdom Chidera <heisdera@gmail.com>
2025-05-13 03:52:57 +0100

    fix: fix vercel build error by adding suspense fallback loader for product list rendering

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,13 @@
+'use client'
+
 import { ProductsList } from '@/components/ProductList'
+import { Loader2 } from 'lucide-react'
+import { Suspense } from 'react'
 
 export default function Page() {
-  return <ProductsList />
+  return (
+    <Suspense fallback={<Loader2 className="size-6 animate-spin" />}>
+      <ProductsList />
+    </Suspense>
+  )
 }
